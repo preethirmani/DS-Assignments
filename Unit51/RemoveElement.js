8a9e7a3595af547b0fce911ab09fe43fa12ad522
Preethi <writetopreethir@gmail.com>
2024-04-15 11:40:09 -0700

    Remove Element

---
+++ Unit51/RemoveElement.js
@@ -0,0 +1,10 @@
+/**
+ * Remove nodes with a specific value from a linked list. The given linked list has a head node [1,2,6,3,4,5,6] and the specified value is 6. The expected output is a modified list with nodes [1,2,3,4,5]. 
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}

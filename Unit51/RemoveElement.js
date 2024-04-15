abcf0e9273786cdcab426fd8868a9bf9e4fabbb8
Preethi <writetopreethir@gmail.com>
2024-04-15 11:47:17 -0700

    recursion remove element

--- Unit51/RemoveElement.js
+++ Unit51/RemoveElement.js
@@ -8,3 +8,25 @@ class Node {
     this.next = null;
   }
 }
+
+const linked_list = new Node(1);
+linked_list.next = new Node(2);
+linked_list.next.next = new Node(6);
+linked_list.next.next.next = new Node(3);
+linked_list.next.next.next.next = new Node(4);
+linked_list.next.next.next.next.next = new Node(5);
+linked_list.next.next.next.next.next.next= new Node(6);
+
+console.log(linked_list);
+
+function removeElement(head, val) {
+  if(!head) return null;
+  if(head.val === val) {
+     return removeElement(head.next, val);
+  }
+  head.next = removeElement(head.next, val);
+  return head;
+}
+
+console.log(removeElement(linked_list,6));
+console.log(linked_list.next.next);
